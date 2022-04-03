796041ac5a00542797a3532a0d9e516ca91c079a
SamirDhakal <68449594+SamirDhakal@users.noreply.github.com>
2022-04-03 18:40:46 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+let latitude = 22.7868542, longitude = 88.3643296;
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2FtaXJzYW1pciIsImEiOiJjbDBuM3Vmd2oxY3Z1M2pzNWd1M2cyZDg5In0.wXxhHB0uWTKqVfha5wHKMw';
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: [longitude, latitude],
+    zoom: 4
+})
+
+var img1 = document.querySelector("#amber");
+
+var marker1 = new mapboxgl.Marker({
+    element: img1
+})
+.setLngLat([75.85133, 26.98547]).addTo(map);
+
+map.addControl(
+    new MapboxGenerator({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl
+    })
+)
